6ceca8101edccdc2c931d3d64961d89a63d958d5
onestone23 <lws2269@kakao.com>
2024-10-22 13:49:59 +0900

    feat: userProducts

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,5 +2,24 @@ import { useState } from 'react';
 import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const updateProduct = (product: Product) => {
+    const prevProduct = products.find((item) => item.id === product.id);
+    if (prevProduct) {
+      setProducts((prev) => prev.map((item) => (item.id === product.id ? { ...product } : item)));
+    } else {
+      addProduct(product);
+    }
+  };
+
+  const addProduct = (product: Product) => {
+    setProducts((prev) => [...prev, product]);
+  };
+
+  return {
+    products,
+    updateProduct,
+    addProduct,
+  };
 };
